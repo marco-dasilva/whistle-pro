72a13d233373cb2a11ce1683ceee9ca5294168e4
Marco Da Silva <marco07d@gmail.com>
2020-03-04 11:29:44 -0500

    Switch us to angular over react

--- packages/app/queries/Login.tsx
+++
@@ -1,9 +0,0 @@
-import { gql } from 'apollo-boost';
-
-export const GET_TOKEN = gql`
-  mutation Login($email: String!, $password: String!) {
-    login(login:{ email: $email, password: $password}) {
-      token
-    }
-  }
-`;
